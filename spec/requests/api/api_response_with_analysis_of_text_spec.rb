344818993786fd658e0f69b03b323dcd324c114f
kim_haaga <kim.haaga@live.se>
2021-04-22 20:47:41 +0200

    Add profanity text test

--- spec/requests/api/api_response_with_analysis_of_text_spec.rb
+++ spec/requests/api/api_response_with_analysis_of_text_spec.rb
@@ -23,4 +23,24 @@ RSpec.describe 'POST /api/analyses', type: :request do
       expect(JSON.parse(response.body)['results']['classifications']).to eq expected_output
     end
   end
+
+  describe 'Profanity text analysis' do
+    before do
+      post '/api/analyses', params: {
+        analysis: {
+          resource: 'Fuck you boo',
+          category: 'text'
+        }
+      }
+    end
+
+    it 'is expected to return text resource' do
+      expect(JSON.parse(response.body)['results']['text']).to eq 'Fuck you boo'
+    end
+  
+    it 'is expected to return profanity text analysis' do
+      expected_output = "[{\"tag_name\"=>\"profanity\", \"tag_id\"=>57725627, \"confidence\"=>0.96}]"
+      expect(JSON.parse(response.body)['results']['classifications']).to eq expected_output
+    end
+  end
 end 
